71916bf37a90f67471a0c39dd04d2c0d2f46245c
Bobby Kostadinov <Bobby.Kostadinov@dowjones.com>
2017-02-24 15:47:00 +0000

    aggregation using two obserables

--- build/client/index.js
+++ build/client/index.js
@@ -4,17 +4,37 @@ var _Rx = require('rxjs/Rx');
 
 var _Rx2 = _interopRequireDefault(_Rx);
 
+var _jquery = require('jquery');
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 console.log('start');
 
-var btn = document.getElementById("addFrameBtn");
-var rolls$ = _Rx2.default.Observable.fromEvent(btn, 'click').do(function (e) {
+var updateBoard = function updateBoard(score) {
+  var $new = (0, _jquery2.default)('.frame li:last').clone();
+  $new.find('.badge').text(score);
+  $new.find('.label').removeClass('label-primary');
+  $new.find('.label').addClass('label-default');
+  $new.find('.label').text('Frame Score');
+  (0, _jquery2.default)('.frame').append($new);
+};
+
+var updateTotalScore = function updateTotalScore(score) {
+  var $first = (0, _jquery2.default)('.frame li:first');
+  $first.find('.badge').text(score);
+};
+
+var form = (0, _jquery2.default)("#addFrameBtn");
+var rolls$ = _Rx2.default.Observable.fromEvent(form, 'submit').do(function (e) {
   return e.preventDefault();
-}).subscribe(function (x) {
-  console.log('Submit: Clicked!');
-}, function (err) {
-  console.log('Error: %s', err);
-}, function () {
-  console.log('Completed');
+}).map(function (_) {
+  return parseInt(form.find('#roll1').val()) + parseInt(form.find('#roll2').val());
 });
+
+rolls$.subscribe(updateBoard);
+
+var endGame$ = rolls$.scan(function (x, y) {
+  return x + y;
+}, 0).subscribe(updateTotalScore);
--- src/client/index.js
+++ src/client/index.js
@@ -1,17 +1,32 @@
 import Rx from 'rxjs/Rx';
+import $ from 'jquery'
 
 console.log('start');
 
-const btn = document.getElementById("addFrameBtn");
-const rolls$ = Rx.Observable.fromEvent(btn, 'click')
+const updateBoard = score => {
+  const $new = $('.frame li:last').clone();
+  $new.find('.badge').text(score)
+  $new.find('.label').removeClass('label-primary');
+  $new.find('.label').addClass('label-default');
+  $new.find('.label').text('Frame Score');
+  $('.frame').append($new);
+}
+
+const updateTotalScore = score => {
+  const $first = $('.frame li:first');
+  $first.find('.badge').text(score)
+}
+
+
+const form = $("#addFrameBtn");
+const rolls$ = Rx.Observable.fromEvent(form, 'submit')
   .do(e => e.preventDefault())
-  .subscribe(
-  function (x) {
-      console.log('Submit: Clicked!');
-    },
-    function (err) {
-      console.log('Error: %s', err);
-    },
-    function () {
-      console.log('Completed');
-  });
+  .map(_ =>
+    parseInt(form.find('#roll1').val()) + parseInt(form.find('#roll2').val())
+  );
+
+
+  rolls$.subscribe(updateBoard);
+
+  const endGame$ = rolls$.scan((x, y) => x + y, 0)
+    .subscribe(updateTotalScore)
